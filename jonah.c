bd84ecf05f846f0ab0715c735e3ffe77fcd49546
ryanbs183 <ryan.sullivan.183@gmail.com>
2021-10-06 20:16:18 -0400

    add in empty function defs

--- jonah.c
+++ jonah.c
@@ -6,3 +6,17 @@ int init(void) {
 	// attach BPF programs
 	fprintf("starting jonah...");
 }
+
+void filter(char* line) {
+	/* filter through line of data
+	if the line is part of the docker build no change
+	else set line = "" */
+}
+
+void check_hooks() {
+	// checks eBPF hook buffers
+}
+
+void monitor_procs() {
+	// process monitor for Docker build
+}
