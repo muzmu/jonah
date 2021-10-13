4879c465e3750d95fa11bfc49c6ef1aa52b11ebb
ryanbs183 <ryan.sullivan.183@gmail.com>
2021-10-13 19:50:51 -0400

    use jonah.py to mount BPF programs

---
+++ jonah.py
@@ -0,0 +1,19 @@
+# Possibly better mounter for eBPF programs
+# could use C to make as ligthweight a signal handler and filter
+# okay to use python for initial muont since should only be run once
+
+import sys
+import time
+
+from bcc import BPF
+from bcc.utils import printb
+
+# load BPF programs into the program
+
+bpf_create = BPF(src_file="bpf_progs/creat_prog.c")
+bpf_mod = BPF(src_file="bpf_progs/mod_prog.c")
+bpf_net_in = BPF(src_file="bpf_progs/net_in_prog.c")
+bpf_net_out = BPF(src_file="bpf_progs/net_out_prog.c")
+
+bpf_create.attach_kprobe(event="vfs_create", fn_name="creat_watch")
+
