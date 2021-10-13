42c3de59b19a5b6653f3d1501c15f4d8cd49eea8
ryanbs183 <ryan.sullivan.183@gmail.com>
2021-10-13 19:52:43 -0400

    update jonah.c

--- jonah.c
+++ jonah.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 #include <signal.h>
 
-#include <linux/bpf.h>
-#include <test_progs.h>
-
-#define FILE_CREAT 	"bpf_progs/creat_prog.o"
-#define FILE_MOD 	"bpf_progs/mod_prog.o"
-#define NET_IN		"bpf_progs/net_in_prog.o"
-#define NET_OUT		"bpf_progs/net_out_prog.o"
-
 int init(void) {
-	// attach BPF programs defined above **look into renaming
-	int creat_progfd, mod_progfd, 
-	net_in_progfd, net_out_progfd;
-
-	struct bpf_object *creat_obj, *mod_obj, 
-					  *net_in_obj, *net_out_obj;
-	
-	if(bpf_prog_load(FILE_CREAT, BPF_PROG_TYPE_))
-	
-
 	// register signal handlers
 
 	// register daemon to handle reading buffers
