427929779789ff02b5d510ebea8eaded8824cb4e
Ryan Sullivan <ryanbakersullivan@gmail.com>
2021-10-27 23:50:01 +0000

    clean up network monitor code

--- bpf_progs/bpf_fileops.c
+++ bpf_progs/bpf_fileops.c
@@ -20,36 +20,21 @@ int do_read(struct pt_regs *ctx,struct file *file){
 	struct data_t data = {};
 	struct task_struct *t = (struct task_struct *)bpf_get_current_task();
 	struct path *p = &(t->fs->pwd);
-	
-
 	u32 pid;
-    	if (!PT_REGS_RC(ctx)){
-    		return 0;
-	}
+    if (!PT_REGS_RC(ctx))
+    	return 0;
+
  	struct dentry *de = file->f_path.dentry;
 	int mode = file->f_inode->i_mode;
 	struct qstr d_name = de->d_name;
-		
-		bpf_probe_read_kernel(&data.filename, sizeof(data.filename), d_name.name);
+	bpf_probe_read_kernel(&data.filename, sizeof(data.filename), d_name.name);
 	
-   
 	pid = bpf_get_current_pid_tgid() >> 32;
-    	data.pid = pid;
+    data.pid = pid;
 
 	strcpy(data.str, "read");
-	int i = 0;
-	/*
-	for(i; i < TASK_COMM_LEN; i++){
-		data.filename[i] = (p->dentry->d_name.name)[i];
-		if((p->dentry->d_iname)[i+1] == '\0')
-			break;
-	}*/
-	
-	//strcpy(data.filename, (char*)(p->dentry->d_iname));
-	//sprintf(data.filename, "%lu", p->dentry->d_time);
 
 	bpf_get_current_comm(&(data.comm), sizeof(data.comm));
-	
 	events.perf_submit(ctx, &data, sizeof(data));
 
 	return 0;
@@ -63,23 +48,15 @@ int do_write(struct pt_regs *ctx,struct file *file){
     if (!PT_REGS_RC(ctx))
     	return 0;
 
-    	struct dentry *de = file->f_path.dentry;
-        int mode = file->f_inode->i_mode;
-        struct qstr d_name = de->d_name;
-        bpf_probe_read_kernel(&data.filename, sizeof(data.filename), d_name.name);
+    struct dentry *de = file->f_path.dentry;
+    int mode = file->f_inode->i_mode;
+    struct qstr d_name = de->d_name;
+	bpf_probe_read_kernel(&data.filename, sizeof(data.filename), d_name.name);
     
 	pid = bpf_get_current_pid_tgid() >> 32;
     data.pid = pid;
 
 	strcpy(data.str, "write");
-	int i = 0;
-	/*
-	for(i; i < TASK_COMM_LEN; i++){
-		data.filename[i] = (p->dentry->d_name.name)[i];
-		if((p->dentry->d_iname)[i+1] == '\0')
-			break;
-	}
-	*/
 
 	bpf_get_current_comm(&(data.comm), sizeof(data.comm));
 	events.perf_submit(ctx, &data, sizeof(data));
@@ -96,21 +73,14 @@ int do_open(struct pt_regs *ctx,struct file *file){
     	return 0;
 
     struct dentry *de = file->f_path.dentry;
-        int mode = file->f_inode->i_mode;
-        struct qstr d_name = de->d_name;
-        bpf_probe_read_kernel(&data.filename, sizeof(data.filename), d_name.name);
+	int mode = file->f_inode->i_mode;
+	struct qstr d_name = de->d_name;
+	bpf_probe_read_kernel(&data.filename, sizeof(data.filename), d_name.name);
     
 	pid = bpf_get_current_pid_tgid() >> 32;
     data.pid = pid;
 
 	strcpy(data.str, "open");
-	int i = 0;
-	/*
-	for(i; i < TASK_COMM_LEN; i++){
-		data.filename[i] = (p->dentry->d_name.name)[i];
-		if((p->dentry->d_iname)[i+1] == '\0')
-			break;
-	}*/
 
 	bpf_get_current_comm(&(data.comm), sizeof(data.comm));
 	events.perf_submit(ctx, &data, sizeof(data));
@@ -127,21 +97,14 @@ int do_create(struct pt_regs *ctx,struct file *file){
     	return 0;
 
     struct dentry *de = file->f_path.dentry;
-        int mode = file->f_inode->i_mode;
-        struct qstr d_name = de->d_name;
-        bpf_probe_read_kernel(&data.filename, sizeof(data.filename), d_name.name);
+	int mode = file->f_inode->i_mode;
+	struct qstr d_name = de->d_name;
+	bpf_probe_read_kernel(&data.filename, sizeof(data.filename), d_name.name);
     
 	pid = bpf_get_current_pid_tgid() >> 32;
     data.pid = pid;
 
 	strcpy(data.str, "create");
-	int i = 0;
-	/*
-	for(i; i < TASK_COMM_LEN; i++){
-		data.filename[i] = (p->dentry->d_name.name)[i];
-		if((p->dentry->d_iname)[i+1] == '\0')
-			break;
-	}*/
 
 	bpf_get_current_comm(&(data.comm), sizeof(data.comm));
 	events.perf_submit(ctx, &data, sizeof(data));
--- bpf_progs/bpf_netops.c
+++ bpf_progs/bpf_netops.c
@@ -48,7 +48,7 @@ int do_tcpv4(struct pt_regs *ctx, struct sock *sk){
     struct inet_sock *sockp = (struct inet_sock *)skp;
     u32 saddr = 0;
     bpf_probe_read_kernel(&saddr, sizeof(saddr), &sockp->inet_saddr);
-    //u32 saddr = sk->__sk_common.skc_daddr;
+    
     u32 pid;
     pid = bpf_get_current_pid_tgid() >> 32;
     data.pid = pid;
@@ -64,16 +64,20 @@ int do_tcpv4(struct pt_regs *ctx, struct sock *sk){
 
 int do_tcpv6(struct pt_regs *ctx, struct sock *sk){
     struct data_t data = {};
-    u32 saddr = sk->__sk_common.skc_daddr;
+    struct sock *skp = sk;
+    struct inet_sock *sockp = (struct inet_sock *)skp;
+    u32 saddr = 0;
+    bpf_probe_read_kernel(&saddr, sizeof(saddr), &sockp->inet_saddr);
+    
     u32 pid;
     pid = bpf_get_current_pid_tgid() >> 32;
     data.pid = pid;
 
-    strcpy(data.op, "TCP IPv6");
+    strcpy(data.op, "TCP IPv4");
     data.addr = saddr;
     bpf_get_current_comm(&(data.comm), sizeof(data.comm));
 
-    tcpv4_events.perf_submit(ctx, &data, sizeof(data));
+    tcpv6_events.perf_submit(ctx, &data, sizeof(data));
     
     return 0;
 }
