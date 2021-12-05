f2c4e10cb433dec91fed75f4917a6b157b2452ec
muzmu <muz@bu.edu>
2021-12-05 22:18:24 +0000

    Most of the errors fixed and the ring buffer issue resolved

--- jonah.py
+++ jonah.py
@@ -12,31 +12,42 @@ from socket import (
 import socket
 from struct import pack
 from collections import defaultdict
+import time
 argv = defaultdict(list)
 
 log = open("/home/fedora/muz/jonah/jonah.log", "w+")
 
-bpf_ops = BPF(src_file="bpf_progs/bpf_ops.c")
+#bpf_ops = BPF(src_file="bpf_progs/bpf_ops.c")
+execv_ops = BPF(src_file="bpf_progs/execv.c")
+tcp4 = BPF(src_file="bpf_progs/tcp4.c")
+tcp6 = BPF(src_file="bpf_progs/tcp6.c")
+read= BPF(src_file="bpf_progs/read.c")
+write= BPF(src_file="bpf_progs/write.c")
 
-execve_fnname = bpf_ops.get_syscall_fnname("execve")
-bpf_ops.attach_uretprobe(name='/bin/bash',sym="readline", fn_name="printret")
-bpf_ops.attach_kprobe(event=execve_fnname, fn_name="syscall__execve")
-bpf_ops.attach_kprobe(event="vfs_read",   fn_name="do_read")
-bpf_ops.attach_kprobe(event="vfs_write",  fn_name="do_write")
-bpf_ops.attach_kprobe(event="vfs_open",   fn_name="do_open")
-bpf_ops.attach_kprobe(event="vfs_create", fn_name="do_create")
+execve_fnname = execv_ops.get_syscall_fnname("execve")
+#bpf_ops.attach_uretprobe(name='/bin/bash',sym="readline", fn_name="printret")
+execv_ops.attach_kprobe(event=execve_fnname, fn_name="syscall__execve")
+read.attach_kprobe(event="vfs_read",   fn_name="do_read")
+write.attach_kprobe(event="vfs_write",  fn_name="do_write")
+#file_ops.attach_kprobe(event="vfs_open",   fn_name="do_open")
+#file_ops.attach_kprobe(event="vfs_create", fn_name="do_create")
 
 
-bpf_ops.attach_kprobe(event="tcp_v4_connect", fn_name="do_tcpv4")
-bpf_ops.attach_kprobe(event="tcp_v6_connect", fn_name="do_tcpv6")
+tcp4.attach_kprobe(event="tcp_v4_connect", fn_name="do_tcpv4")
+tcp6.attach_kprobe(event="tcp_v6_connect", fn_name="do_tcpv6")
 #bpf_ops.attach_kretprobe(event="tcp_v6_disconnect", fn_name="do_tcpv6")
 write_file_dict = defaultdict(list);
 read_file_dict = defaultdict(list);
 tcp4_dict = defaultdict(list);
 tcp6_dict = defaultdict(list);
 
-def log_file_event(cpu, data, size):
-    event = bpf_ops["events"].event(data)
+def log_read_event():
+    time.sleep(4)
+    counts = read.get_table("counts")
+    #for key,value in counts.items():
+     #   print(key.pid,key.filename)
+
+    '''event = read["read"].event(data)
     filename = str(event.filename.decode('utf-8', 'replace'))
     event_type = str(event.str.decode('utf-8', 'replace'))
     pid = int(event.pid)
@@ -46,18 +57,40 @@ def log_file_event(cpu, data, size):
     except:
         a=1
     #print(event_type,filename)
-    if event_type == 'write':
-        write_file_dict[pid].append(filename)
-    if event_type == 'read':
-        read_file_dict[pid].append(filename)
+    
+    read_file_dict[pid].append(filename)
     e = "PID: %s \t OP: %s \t NAME: %s \t FILE: %s \n" % (event.pid, event.str.decode(
         'utf-8', 'replace'), event.comm.decode('utf-8', 'replace'), event.filename.decode('utf-8', 'replace'))#, event.path_dir.decode('utf-8', 'replace'))
     #print(e)
     #log.write(e)
-    #log.flush()
+    #log.flush()'''
+
+
+def log_write_event():
+    time.sleep(4)
+    counts = write.get_table("counts")
+    for key,value in counts.items():
+        print(key.pid,key.filename)
+        
+    '''event = write["write"].event(data)
+    filename = str(event.filename.decode('utf-8', 'replace'))
+    event_type = str(event.str.decode('utf-8', 'replace'))
+    pid = int(event.pid)
+    try:
+        f = open("/proc/"+str(pid)+"/cmdline")
+        #print(f.readline())
+    except:
+        a=1
+    #print(event_type,filename)
+    write_file_dict[pid].append(filename)
+    e = "PID: %s \t OP: %s \t NAME: %s \t FILE: %s \n" % (event.pid, event.str.decode(
+        'utf-8', 'replace'), event.comm.decode('utf-8', 'replace'), event.filename.decode('utf-8', 'replace'))#, event.path_dir.decode('utf-8', 'replace'))
+   # print(e)
+    #log.write(e)
+    #log.flush()'''
 
 def log_execv_event(cpu, data, size):
-    event = bpf_ops["execv_events"].event(data)
+    event = execv_ops["execv_events"].event(data)
     argv[event.pid].append(event.argv)
 
     #print(argv)
@@ -66,40 +99,61 @@ def log_execv_event(cpu, data, size):
     #log.write(e)
     #log.flush()
 
-
 def log_tcpv4_event(cpu, data, size):
-    tcp4_dict[event.pid].append(inet_ntop(AF_INET, pack("I", event.addr)).encode())
-    event = bpf_ops["tcpv4_events"].event(data)
+    event = tcp4["tcpv4_events"].event(data)
     e = "PID: %d \t OP: %s \t NAME: %s \t ADDR: %-39s \n" % (event.pid, event.op.decode(
         'utf-8', 'replace'), event.comm.decode('utf-8', 'replace'), inet_ntop(AF_INET, pack("I", event.addr)).encode())
-    print(e)
+    #print(e)
+    tcp4_dict[event.pid].append(inet_ntop(AF_INET, pack("I", event.addr)).encode())
+   # print(tcp4_dict)
     #log.write(e)
     #log.flush()
 
 def log_tcpv6_event(cpu, data, size):
-    event = bpf_ops["tcpv6_events"].event(data)
+    event = tcp6["tcpv6_events"].event(data)
     tcp6_dict[event.pid].append(inet_ntop(AF_INET, pack("I", event.addr)).encode())
     e = "PID: %d \t OP: %s \t NAME: %s \t ADDR: %d \n" % (event.pid, event.op.decode(
         'utf-8', 'replace'), event.comm.decode('utf-8', 'replace'), event.addr)
-    print(e)
+    #print(e)
+    #print(tcp6_dict)
     #log.write(e)
     #log.flush()
 
 def log_cmdline(cpu,data,size):
     event=bpf_ops["cmd"].event(data)
-    print(event.pid,event.str)
+    #print(event.pid,event.str)
 
-bpf_ops["cmd"].open_perf_buffer(log_cmdline)
-bpf_ops["events"].open_perf_buffer(log_file_event)
-bpf_ops["tcpv4_events"].open_perf_buffer(log_tcpv4_event)
-bpf_ops["tcpv6_events"].open_perf_buffer(log_tcpv6_event)
-bpf_ops["execv_events"].open_perf_buffer(log_execv_event)
+#bpf_ops["cmd"].open_perf_buffer(log_cmdline)
+#read["read"].open_perf_buffer(log_read_event)
+#write["write"].open_perf_buffer(log_write_event)
+tcp4["tcpv4_events"].open_perf_buffer(log_tcpv4_event)
+tcp6["tcpv6_events"].open_perf_buffer(log_tcpv6_event)
+execv_ops["execv_events"].open_perf_buffer(log_execv_event)
 
-def log_thread():
+def log_read_ops_thread():
     while True:
-        bpf_ops.perf_buffer_poll()
+        log_read_event()
 
-_thread.start_new_thread(log_thread, ())
+
+def log_write_ops_thread():
+    while True:
+        log_write_event()
+
+def log_execv_ops_thread():
+    while True:
+        execv_ops.perf_buffer_poll()
+def log_tcp4_thread():
+    while True:
+        tcp4.perf_buffer_poll()
+def log_tcp6_thread():
+    while True:
+        tcp6.perf_buffer_poll()
+
+_thread.start_new_thread(log_read_ops_thread, ())
+_thread.start_new_thread(log_write_ops_thread, ())
+_thread.start_new_thread(log_execv_ops_thread, ())
+_thread.start_new_thread(log_tcp4_thread, ())
+_thread.start_new_thread(log_tcp6_thread, ())
 
 print("STARTING DOCKER\n")
 while True:
