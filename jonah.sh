2355b73d864a07f96ac9606fbea4d236a9906ab5
Ryte <ryterichard@gmail.com>
2021-12-07 11:27:50 -0500

    Update jonah.sh

--- jonah.sh
+++ jonah.sh
@@ -11,4 +11,4 @@ REGISTRY="localhost:5000"
 
 sudo docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} .
 sudo cp /jonah/jonah.log jonah.log
-sudo oras push ${REGISTRY}/${IMAGE_NAME}:${TAG} --manifest-config /dev/null:application/vnd.oci.jonah.config jonah.log:text/plain
+sudo oras push ${REGISTRY}/${IMAGE_NAME}:${TAG} --manifest-config /dev/null:application/vnd.oci.jonah.config jonah.log
